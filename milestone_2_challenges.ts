8454f16b1d5af1b41b46e682a84819e23e8e77ae
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-06-02 15:43:52 +0300

    sums even numbers

--- milestone_2_challenges.ts
+++ milestone_2_challenges.ts
@@ -78,3 +78,18 @@ function filterByLength(strings: string[], minLength: number){
  })
 }
 console.log(filterByLength(["cat", "giraffe", "hippo", "dog", "elephant"], 5));
+
+// Challenge 7 :Sum of Even Numbers
+
+function sumEvenNumbers(arr: number[]){
+ let sum=0;
+
+ for (let i=0; i<arr.length; i++){
+   if (arr[i]%2 ==0){
+     sum+=arr[i];
+   }
+ }
+return sum;
+}
+
+console.log (sumEvenNumbers([1, 2, 3, 4, 5, 6]));
